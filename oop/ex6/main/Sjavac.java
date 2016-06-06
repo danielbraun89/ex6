d96bd65a1b272ab1c43c42189c9f10e941e59cf7
danielbraun89 <braundaniel@live.com>
2016-06-06 12:01:52 +0300

    ertsfzdsafa

---
+++ oop/ex6/main/Sjavac.java
@@ -0,0 +1,7 @@
+package ex6.main;
+
+/**
+ * Created by Daniel on 05/06/2016.
+ */
+public class Sjavac {
+}
---
+++ oop/ex6/main/testClass.java
@@ -0,0 +1,7 @@
+package ex6.main;
+
+/**
+ * Created by Daniel on 06/06/2016.
+ */
+public class testClass {
+}
