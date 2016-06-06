05cc56413309b63564589791eb1fcd36fead6fb8
danielbraun89 <braundaniel@live.com>
2016-06-06 18:42:24 +0300

    ertsfzdsafa

--- oop/ex6/main/Sjavac.java
+++ oop/ex6/main/Sjavac.java
@@ -1,7 +1,35 @@
 package ex6.main;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
 
-/**
- * Created by Daniel on 05/06/2016.
- */
+
+
 public class Sjavac {
+    public static void main(String[] argv) {
+        List<String> lineList;
+        Path filepath = Paths.get(argv[0]);
+        try {
+            lineList = Files.readAllLines(filepath);
+            for (String line : lineList){
+                String caseOfAction = check(line);
+                switch(caseOfAction){
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                }
+            }
+        }catch(SyntaxException x){
+        }catch(IOException x){
+
+        }
+    }
+    private static String check(String line){
+
+    }
 }
